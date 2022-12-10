c6af58f9867dafe293e2a365ad271d787c1f5345
Aleks Atanasov <alex2002atanasov@abv.bg>
2022-12-10 02:57:12 +0200

    First commit

---
+++ src/app/nav/navbar.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input } from '@angular/core'
+import { AuthService } from '../user/auth.service'
+
+@Component ({ 
+    selector: 'nav-bar',
+    templateUrl: './navbar.component.html',
+    styles: [`
+    .nav.navbar-nav { font-size: 15px;}
+    #searchForm { margin-right: 100px;}
+    @media (max-width: 1200px) {#searchForm {display: none}}
+    li > a.active { color: #F97924; }
+`]
+})
+
+export class NavBarComponent {
+constructor(public auth: AuthService) {
+}
+}
